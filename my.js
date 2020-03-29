7e1df8b72f8172393dbee41f795a696e6afe7109
marco <dobermannpoker86@gmail.com>
2020-03-29 16:17:12 +0200

    random carrozze

--- my.js
+++ my.js
@@ -17,18 +17,28 @@ var bottoneGenerator = document.getElementById("genera");
     var putName = urName.value;
     var putKm = urKm.value;
     var putAge = urAge.value;
+    //stampo nome
     var stampoPrimo = document.getElementById("passeggero");
-    var stampoSecondo = document.getElementById("");
-    //prezzo
-    var stampoTerzo = document.getElementById("prezzo");
-    stampoPrimo.innerHTML = putName;
+        stampoPrimo.innerHTML = putName;
+//random carrozza
+        var carrozza = document.getElementById("carrozza");
+        carrozza.innerHTML = Math.floor((Math.random("a caso")* 30) );
+
+//
+    var stampoSecondo = document.getElementById("prezzo");
+    stampoSecondo.innerHTML= urKm.value;
+
+    if (urKm.value <200) {
+        stampoSecondo.innerHTML= (offerta * urKm)/100;
+
+    }
     //offerta
     var offerta = document.getElementById("offerta");
     offerta.innerHTML= urAge.value;
     if (urAge.value <= 18) {
-        offerta.innerHTML = "sconto10";
+        offerta.innerHTML = "sconto10%";
     }else if (urAge.value > 55) {
-        offerta.innerHTML = "sconto15";
+        offerta.innerHTML = "sconto15%";
     }else {
         offerta.innerHTML = "prezzo standard";
     }
